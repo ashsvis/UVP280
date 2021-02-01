bd81dd74fd32230ef8879d68ff69db5f83c3234d
ashsvis <ashsvis@gmail.com>
2021-02-01 14:29:43 +0300

    Подправил

--- Program.cs
+++ Program.cs
@@ -380,7 +380,7 @@ namespace UVP280
 
         public override string ToString()
         {
-            return string.Format("{0}{1:0000}{5} {4,-42} {2,15} {3,-10}", Func, Addr, Value, Unit, Desc, TypeValue.ToString()[0]);
+            return string.Format("{0}{1:0000}{5} {4,-42} {2,15} {3,-11}", Func, Addr, Value, Unit, Desc, TypeValue.ToString()[0]);
         }
     }
 }
